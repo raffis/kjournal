12fea71267f9a7f284bf9bfe340d1e8e2d5301ea
Raffael Sahli <raffael.sahli@doodle.com>
2022-10-26 23:20:59 +0000

    fix: dump test each time
    Former-commit-id: 85bf79a3e7ae14bb9e6e85a757949732c9edf22e

--- config/tests-base/validation/tests.sh
+++ config/tests-base/validation/tests.sh
@@ -4,5 +4,4 @@ export timeout=250
 export retry=3
 
 echo "# Find logs from log-generator in no stream mode succeeds"
-echo '{"foobar": "foobar", "message": "dummy log message"}'
-timeout $timeout /bin/ash -c "until kjournal pods -n kjournal-system log-generator --no-stream -o json --field-selector payload.foobar=foobar | grep dummy; do sleep $retry; done"
+timeout $timeout /bin/ash -c "until echo '{\"foobar\": \"foobar\", \"message\": \"dummy log message\"}'; kjournal pods -n kjournal-system log-generator --no-stream -o json --field-selector payload.foobar=foobar | grep dummy; do sleep $retry; done"
