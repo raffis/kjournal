7da55bfe55bcbe538eeaf88fb2d3f4cf684e83af
Raffael Sahli <raffael.sahli@doodle.com>
2022-11-17 21:02:22 +0000

    fix: debug log

--- cli/pkg/manifestgen/doc.go
+++ cli/pkg/manifestgen/doc.go
@@ -1,3 +1,3 @@
-// Note: Most parts here have been borrowed from flux2, https://github.com/fluxcd/flux2. Thanks!
+// Note: Most parts of this package have been borrowed from flux2, https://github.com/fluxcd/flux2. Thanks!
 
 package manifestgen
